bbd7ab66aa8a722cbc066d86540ec258a13c5282
Matheus Bloize <mbloizepd@gmail.com>
2024-08-24 16:56:45 -0300

    feat: create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>
+        <p>Copyright @ 2024</p>
+        <div>-</div>
+        <p>Todos os direitos reservados</p>
+      </span>
+      <p>Projeto fictício de estudo</p>
+    </footer>
+  );
+};
+
+export default Footer;
